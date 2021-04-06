fcbee8c06fbad40e1e9974415fbcc4d937a719ae
Maky <franklinjaradev@gmail.com>
2021-04-06 19:34:31 -0400

    Skeleton

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+int main() { return 0; }
+
+void menu(int choice) {}
+
+void selectionSort();
+
+void insertionSort();
+
+void bubbleSort();
+
+void mergeSort();
+
+void quickSort();
+
+void heapSort();
