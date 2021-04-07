6da3ba347e06648c63672ec4411cb0b1dcd66c3d
Maky <franklinjaradev@gmail.com>
2021-04-07 12:10:49 -0400

    Added random number generator, and selection sort algorithm

--- main.cpp
+++ main.cpp
@@ -1,13 +1,90 @@
 #include <iostream>
+#include <stdlib.h>
 #include <time.h>
-#include <vector>
+
+void printUnsortedArray(int numbers[]) {
+
+  std::cout << "UNSORTED ARRAY" << std::endl;
+
+  for (int i = 0; i < 25; i++) {
+    std::cout << numbers[i] << " ";
+  }
+  std::cout << std::endl;
+}
+
+void printSortedArray(int numbers[]) {
+
+  std::cout << "SORTED ARRAY" << std::endl;
+
+  for (int i = 0; i < 25; i++) {
+    std::cout << numbers[i] << " ";
+  }
+  std::cout << std::endl;
+}
+void selectionSort(int numbers[], int numbersSize) {
+
+  printUnsortedArray(numbers);
+
+  std::cout << std::endl;
+
+  int i = 0;
+  int j = 0;
+  int indexSmallest = 0;
+
+  // Temp variable for swap
+  int temp = 0;
+
+  for (; i < numbersSize - 1; i++) {
+
+    // Find index of smallest remaining element
+    indexSmallest = i;
+    for (j = i + 1; j < numbersSize; ++j) {
+
+      if (numbers[j] < numbers[indexSmallest]) {
+        indexSmallest = j;
+      }
+    }
+
+    // Swap numbers[i] and numbers[indexSmallest]
+    temp = numbers[i];
+    numbers[i] = numbers[indexSmallest];
+    numbers[indexSmallest] = temp;
+  }
+
+  printSortedArray(numbers);
+}
+
+void insertionSort();
+
+void bubbleSort();
+
+void mergeSort();
+
+void quickSort();
+
+void heapSort();
 
 void menu() {
 
   int choice;
 
+  // For new numbers
+  /* srand(time(0)); */
+
+  int numbers[1000] = {};
+
+  // Populates numbers with random numbers
+  for (int i = 0; i < 1000; i++) {
+    // Random numbers between 0 and 1000
+    numbers[i] = (rand() % 1000 + 1);
+  }
+
+  // Get numbers size
+  int numbersSize = sizeof(numbers) / sizeof(numbers[0]);
+
   do {
 
+    std::cout << std::endl;
     std::cout << "Please choose from the following options - \n";
     std::cout << " 1. Selection Sort. \n";
     std::cout << " 2. Insertion Sort. \n";
@@ -23,6 +100,7 @@ void menu() {
     if (choice == 1) {
 
       std::cout << "SELECTION SORT\n";
+      selectionSort(numbers, numbersSize);
     }
 
     else if (choice == 2) {
@@ -51,18 +129,6 @@ void menu() {
 
   } while (choice != 7);
 }
-
-void selectionSort();
-
-void insertionSort();
-
-void bubbleSort();
-
-void mergeSort();
-
-void quickSort();
-
-void heapSort();
 
 int main() {
 
